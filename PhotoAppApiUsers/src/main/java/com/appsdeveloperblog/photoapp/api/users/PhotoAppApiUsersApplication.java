9b0dc9e671e6125467ed29aa943b8a8882298eaa
nilay <mailnilaydas@gmail.com>
2020-08-31 12:25:26 +0530

    commit

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -0,0 +1,36 @@
+package com.appsdeveloperblog.photoapp.api.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+public class PhotoAppApiUsersApplication {
+
+	@Autowired
+	Environment environment;
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
--- customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -18,6 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableSwagger2
+
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
